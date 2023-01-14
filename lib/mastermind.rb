de807b49f619dbe6257f7f7b72b387adab62d1fc
Rob Hoolan <rhoolan@gmail.com>
2023-01-14 23:42:56 +0900

    add print_matches function

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -4,6 +4,10 @@ class Mastermind
 
     def initialize(length)
         @secret_code = Code.random(length)
-        print @secret_code
+    end
+
+    def print_matches(code)
+        puts "Exact matches: #{@secret_code.num_exact_matches(code)}"
+        puts "Near matches: #{@secret_code.num_near_matches(code)}"
     end
 end
