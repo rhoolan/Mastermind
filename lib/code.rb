6821ddb64e98a2207a766da1a8ec086fc7a7387c
Rob Hoolan <rhoolan@gmail.com>
2023-01-14 20:25:01 +0900

    First commit

---
+++ lib/code.rb
@@ -0,0 +1,32 @@
+class Code
+  attr_reader :pegs
+
+  POSSIBLE_PEGS = {
+    "R" => :red,
+    "G" => :green,
+    "B" => :blue,
+    "Y" => :yellow
+  }
+
+  def self.valid_pegs?(pegs)
+    pegs.all? { |peg| POSSIBLE_PEGS.has_key?(peg.upcase) }
+  end
+
+  def initialize(pegs)
+    if Code.valid_pegs?(pegs)
+      @pegs = pegs.map { |peg| peg.upcase}
+    else
+      raise "Error"
+    end
+  end
+
+  def self.random(length)
+    pegs = Array.new(length) {POSSIBLE_PEGS.keys.sample}
+    Code.new(pegs)
+  end
+
+  def self.from_string(pegs)
+    Code.new(pegs.split(""))
+  end
+
+end
