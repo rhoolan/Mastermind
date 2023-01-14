e704cc8d6defb43ff28927f34ccdc8efef10a876
Rob Hoolan <rhoolan@gmail.com>
2023-01-14 23:46:39 +0900

    add ask_use_for_guess function

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -10,4 +10,12 @@ class Mastermind
         puts "Exact matches: #{@secret_code.num_exact_matches(code)}"
         puts "Near matches: #{@secret_code.num_near_matches(code)}"
     end
+
+    def ask_user_for_guess
+        puts "Enter a code"
+        guess = gets.chomp
+        code = Code.from_string(guess)
+        print_matches(code)
+        @secret_code == code
+    end
 end
